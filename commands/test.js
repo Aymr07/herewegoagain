d4e4f870e8c8828b092ee322ffed8e856a8e25d7
Yousuf Ashraf <Yousuf@Zahras-iMac.local>
2019-12-22 16:34:00 +0400

    added functionality

---
+++ commands/test.js
@@ -0,0 +1,29 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            enabled: true,
+            runIn: ['text', 'dm'],
+            cooldown: 0,
+            description: 'Test message!',
+            quotedStringSupport: false,
+            usage: '',
+            usageDelim: undefined,
+            extendedHelp: 'No extended help available.'
+        });
+    }
+
+    async run(msg, [...params]) {
+        // This is where you place the code you want to run for your command
+    }
+
+    async init() {
+        /*
+         * You can optionally define this method which will be run when the bot starts
+         * (after login, so discord data is available via this.client)
+         */
+    }
+
+};
