84ec0657e5f607722c5c994025bf9f568084b6d2
Yousuf Ashraf <Yousuf@Zahras-iMac.local>
2019-12-30 22:52:48 +0400

    reminders fix. miss smitha jess did this not me

--- tasks/reminders.js
+++ tasks/reminders.js
@@ -3,7 +3,8 @@ const { Task } = require('klasa');
 module.exports = class extends Task {
 
     async run({ channel, user, text }) {
-        const _channel = this.client.channels.get(channel);
+        const _channel = this.client.channels.get(channel.id);
+        console.log(channel)
         return _channel.send(`${user} A new episode of ${text} just released!`);
     }
 
