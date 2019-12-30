cf39c61049265b1219b2e91baa64ae810c4d8b65
Yousuf El Baesy <yousuf5550@gmail.com>
2019-12-31 00:28:44 +0400

    x

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const {
     Client
 } = require('klasa');
 Client.use(require("@kcp/functions"));
+var decrypt = "c0626ebeb5dddf0d5353988bc56a157d0ddd73533660869321e24bbb4dc2d5dc2c8e97cf3b5bd2d26efca0d94eefdbd76ce8b2a0ef71b0a9540b9734f09643fb"
+
+var crypto = require('crypto');
+var mykey = crypto.createDecipher('aes-128-cbc', 'mypassword');
+var mystr = mykey.update(decrypt, 'hex', 'utf8')
+mystr += mykey.final('utf8');
+mystr = mystr.replace("\"", "")
+mystr = mystr.replace("\"", "")
+
+
+console.log(mystr); //abc
 
 new Client({
     fetchAllMembers: false,
@@ -14,6 +25,6 @@ new Client({
         enabled: true
     },
     readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
-}).login("NTYxOTc4NDUyNDkyODEyMzIw.Xf0v1A.3JDhbFl9F9vMmqmljOgwB_zPjhk");
+}).login(mystr);
 
 
